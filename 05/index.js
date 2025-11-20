a8816757da0a79f2925cdacee2eb3a9d27429dbc
AndyPhuHuynh <andyphuhuynh4@gmail.com>
2025-11-19 21:59:18 -0500

    Added ui for matpat

--- 05/index.js
+++ 05/index.js
@@ -2,25 +2,43 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { AnaglyphEffect } from 'three/addons/effects/AnaglyphEffect.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { VertexNormalsHelper } from 'three/addons/helpers/VertexNormalsHelper.js';
 import { Pane } from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.min.js';
 
 // GLOBAL VARIABLES
 
+const loader = new GLTFLoader();
+
 let scene;
 let poly;
 let matpat;
+let matpatVertexHelper;
 let sceneSettings = {
     anaglyph: false,
     isPolyRotating: false,
-    doRotate: function() {
-        this.isPolyRotating = !this.isPolyRotating;
-    }
+    isMatpatRotating: false,
+    blenderOldMaterial: null
 }
+let lights = {
+    ambient: null,
+    directional: null
+};
 let camera;
 let controls;
 let renderer;
 let anaglyph;
 let pane;
+
+const rotations = {};
+rotations.x_180 = new THREE.Quaternion(
+    0,
+    Math.sin(Math.PI / 2) * 0.5,
+    0,
+    Math.cos(Math.PI / 2) * 0.5
+);
+rotations.reset = new THREE.Quaternion(0, 0, 0, 1);
+Object.freeze(rotations)
+
 
 // ---------------------------------
 
@@ -35,12 +53,30 @@ const setupCamera = () => {
 }
 
 const setupLights = () => { 
-    const ambientLight = new THREE.AmbientLight();
-    scene.add(ambientLight);
+    lights.ambient = new THREE.AmbientLight();
+    scene.add(lights.ambient);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
-    directionalLight.position.set(10, 100, 10);
-    scene.add(directionalLight);
+    lights.directional = new THREE.DirectionalLight(0xffffff, 5);
+    lights.directional.position.set(10, 100, 10);
+    scene.add(lights.directional);
+}
+
+const loadModel = (path) => {
+    return new Promise((resolve, reject) => {
+        loader.load(path, (gltf) => {
+            resolve(gltf, undefined, reject);
+        });
+    });
+}
+
+const changeMatpatMaterial = (newMaterial) => {
+    if (sceneSettings.blenderOldMaterial === null) {
+        sceneSettings.blenderOldMaterial = matpat.material.clone();
+        matpat.material = new THREE.MeshNormalMaterial();
+    } else {
+        matpat.material = sceneSettings.blenderOldMaterial;
+        sceneSettings.blenderOldMaterial = null;
+    }
 }
 
 const animate = () => {
@@ -52,40 +88,86 @@ const animate = () => {
         renderer.render(scene, camera);
     }
 
-    if (sceneSettings.poly) {
+    if (poly) {
         if (sceneSettings.isPolyRotating) {
-            sceneSettings.poly.quaternion.slerp
+            poly.quaternion.slerp(rotations.x_180, 0.01);
+        } else {
+            poly.quaternion.slerp(rotations.reset, 0.01);
+        }
+    }
+
+    if (matpat) {
+        if (sceneSettings.isMatpatRotating) {
+            matpat.quaternion.slerp(rotations.x_180, 0.01);
+        } else {
+            matpat.quaternion.slerp(rotations.reset, 0.01);
+        }
+        if (matpatVertexHelper) {
+            matpatVertexHelper.update();
         }
     }
 }
 
-window.onload = () => {
+window.onload = async () => {
     scene = new THREE.Scene();
     setupCamera();
+    setupLights();
 
-    const loader = new GLTFLoader();
-    // Poly
-    loader.load("./models/poly.glb", (gltf) => {
-        poly = gltf.scene;
+    // PolyCam Mesh
+    try {
+        const polyGltf = await loadModel("./models/poly.glb");
+        poly = polyGltf.scenes[0].children[0];
         poly.position.set(3, -2, 0);
         poly.scale.set(20, 20, 20);
         poly.quaternion.set(0, 0, 0, 1);
         scene.add(poly);
-    });
-    // Matpat
-    loader.load("./models/matpat.glb", (gltf) => {
-        matpat = gltf.scene;
-        matpat.position.set(-3, -0.5, 0);
-        matpat.scale.set(2, 2, 2);
+    } catch (error) {
+        alert(`Error loading PolyCam model: ${error}`);
+    }
+
+    // Matpat Mesh
+    try {
+        const matpatGltf = await loadModel("./models/matpat.glb");
+        matpat = matpatGltf.scenes[0].children[0];
+        matpat.position.set(-3, -2, 0);
+        matpat.scale.set(20, 20, 20);
         matpat.quaternion.set(0, 0, 0, 1);
         scene.add(matpat);
-    });
+
+        matpatVertexHelper = new VertexNormalsHelper(matpat, 0.1, "blue");
+        matpatVertexHelper.visible = false;
+        scene.add(matpatVertexHelper);
+    } catch (error) {
+        alert(`Error loading Blender model: ${error}`);
+    }
 
     pane = new Pane();
     let sceneui = pane.addFolder({ title: "Scene" });
     sceneui.addBinding(sceneSettings, "anaglyph");
-    // sceneui.addBinding(matpat.material, "wireframe")
-    sceneui.addButton({title: "Toggle Rotation"}).on("click", () => {sceneSettings.doRotate();});
+    sceneui.addBinding(lights.directional.position, "x", { min: -100, max: 100, step: 1,   label: "Light X" });
+    sceneui.addBinding(lights.directional.position, "y", { min: -100, max: 100, step: 1,   label: "Light Y" });
+    sceneui.addBinding(lights.directional.position, "z", { min: -100, max: 100, step: 1,   label: "Light Z" });
+    sceneui.addBinding(lights.directional, "intensity",  { min: 0,    max: 10,  step: 0.1, label: "Light Intensity" });
+    sceneui.addBinding(lights.ambient, "color", { label: "Ambient Color" });
+
+    if (poly) {
+        let polyui = pane.addFolder({ title: "PolyCam Mesh" });
+        polyui.addBinding(poly.material, "wireframe");
+        polyui.addButton({title: "Toggle Rotation"}).on("click", () => {
+            sceneSettings.isPolyRotating = !sceneSettings.isPolyRotating;
+        });
+    }
+
+    if (matpat) {
+        let matpatui = pane.addFolder({ title: "Blender Mesh" });
+        matpatui.addBinding(matpatVertexHelper, "visible", { label: "Show Vertex Normals" });
+        matpatui.addButton({title: "Toggle Material"}).on("click", () => {
+            changeMatpatMaterial();
+        });
+        matpatui.addButton({title: "Toggle Rotation"}).on("click", () => {
+            sceneSettings.isMatpatRotating = !sceneSettings.isMatpatRotating;
+        });
+    }
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -93,6 +175,5 @@ window.onload = () => {
     anaglyph = new AnaglyphEffect(renderer);
 
     controls = new OrbitControls(camera, renderer.domElement);
-    setupLights();
     animate()
 }
